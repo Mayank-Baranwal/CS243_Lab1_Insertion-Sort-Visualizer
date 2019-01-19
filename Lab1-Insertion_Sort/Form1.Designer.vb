ee4ded7331da3020d53d4e69e40ad960c34a82c3
Mayank Baranwal <baranwalm2002@gmail.com>
2019-01-20 00:44:58 +0530

    GUI Remaining

--- Lab1-Insertion_Sort/Form1.Designer.vb
+++ Lab1-Insertion_Sort/Form1.Designer.vb
@@ -22,9 +22,6 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
-        Me.lblNum = New System.Windows.Forms.Label()
-        Me.txtNum = New System.Windows.Forms.TextBox()
         Me.btnSort = New System.Windows.Forms.Button()
         Me.lblArr = New System.Windows.Forms.Label()
         Me.txtArr = New System.Windows.Forms.TextBox()
@@ -37,44 +34,25 @@ Partial Class Form1
         Me.txtCmp1 = New System.Windows.Forms.TextBox()
         Me.txtCmp2 = New System.Windows.Forms.TextBox()
         Me.txtUnSorted = New System.Windows.Forms.TextBox()
-        Me.lblSorted = New System.Windows.Forms.Label()
-        Me.lblUnsorted = New System.Windows.Forms.Label()
+        Me.lblLegend = New System.Windows.Forms.Label()
         Me.txtCmpRes = New System.Windows.Forms.TextBox()
         Me.lblCmpWith = New System.Windows.Forms.Label()
-        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.txtOutput1 = New System.Windows.Forms.RichTextBox()
         Me.SuspendLayout()
         '
-        'lblNum
-        '
-        Me.lblNum.AutoSize = True
-        Me.lblNum.Location = New System.Drawing.Point(341, 31)
-        Me.lblNum.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.lblNum.Name = "lblNum"
-        Me.lblNum.Size = New System.Drawing.Size(283, 25)
-        Me.lblNum.TabIndex = 0
-        Me.lblNum.Text = "Enter Number of Entities to Sort"
-        '
-        'txtNum
-        '
-        Me.txtNum.Location = New System.Drawing.Point(652, 28)
-        Me.txtNum.Name = "txtNum"
-        Me.txtNum.Size = New System.Drawing.Size(74, 30)
-        Me.txtNum.TabIndex = 1
-        '
         'btnSort
         '
-        Me.btnSort.Location = New System.Drawing.Point(504, 166)
+        Me.btnSort.Location = New System.Drawing.Point(504, 101)
         Me.btnSort.Name = "btnSort"
         Me.btnSort.Size = New System.Drawing.Size(136, 59)
         Me.btnSort.TabIndex = 2
-        Me.btnSort.Text = "Start Insertion Sort"
+        Me.btnSort.Text = "&Start Insertion Sort"
         Me.btnSort.UseVisualStyleBackColor = True
         '
         'lblArr
         '
         Me.lblArr.AutoSize = True
-        Me.lblArr.Location = New System.Drawing.Point(78, 108)
+        Me.lblArr.Location = New System.Drawing.Point(78, 37)
         Me.lblArr.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblArr.Name = "lblArr"
         Me.lblArr.Size = New System.Drawing.Size(110, 25)
@@ -83,18 +61,18 @@ Partial Class Form1
         '
         'txtArr
         '
-        Me.txtArr.Font = New System.Drawing.Font("Calibri", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtArr.Location = New System.Drawing.Point(228, 98)
+        Me.txtArr.Font = New System.Drawing.Font("Calibri", 12.0!)
+        Me.txtArr.Location = New System.Drawing.Point(228, 25)
         Me.txtArr.Multiline = True
         Me.txtArr.Name = "txtArr"
         Me.txtArr.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.txtArr.Size = New System.Drawing.Size(714, 46)
+        Me.txtArr.Size = New System.Drawing.Size(714, 54)
         Me.txtArr.TabIndex = 4
         '
         'lblOutput
         '
         Me.lblOutput.AutoSize = True
-        Me.lblOutput.Location = New System.Drawing.Point(88, 237)
+        Me.lblOutput.Location = New System.Drawing.Point(78, 253)
         Me.lblOutput.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblOutput.Name = "lblOutput"
         Me.lblOutput.Size = New System.Drawing.Size(71, 25)
@@ -103,35 +81,35 @@ Partial Class Form1
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(714, 166)
+        Me.btnExit.Location = New System.Drawing.Point(714, 101)
         Me.btnExit.Name = "btnExit"
         Me.btnExit.Size = New System.Drawing.Size(92, 59)
         Me.btnExit.TabIndex = 7
-        Me.btnExit.Text = "Exit"
+        Me.btnExit.Text = "E&xit"
         Me.btnExit.UseVisualStyleBackColor = True
         '
         'btnClr
         '
-        Me.btnClr.Location = New System.Drawing.Point(331, 166)
+        Me.btnClr.Location = New System.Drawing.Point(331, 101)
         Me.btnClr.Name = "btnClr"
         Me.btnClr.Size = New System.Drawing.Size(92, 59)
         Me.btnClr.TabIndex = 8
-        Me.btnClr.Text = "Clear"
+        Me.btnClr.Text = "&Clear"
         Me.btnClr.UseVisualStyleBackColor = True
         '
         'btnNxt
         '
-        Me.btnNxt.Location = New System.Drawing.Point(504, 166)
+        Me.btnNxt.Location = New System.Drawing.Point(504, 101)
         Me.btnNxt.Name = "btnNxt"
         Me.btnNxt.Size = New System.Drawing.Size(136, 59)
         Me.btnNxt.TabIndex = 9
-        Me.btnNxt.Text = "Next Step"
+        Me.btnNxt.Text = "Next &Step"
         Me.btnNxt.UseVisualStyleBackColor = True
         '
         'lblDone
         '
         Me.lblDone.AutoSize = True
-        Me.lblDone.Location = New System.Drawing.Point(709, 265)
+        Me.lblDone.Location = New System.Drawing.Point(709, 200)
         Me.lblDone.Name = "lblDone"
         Me.lblDone.Size = New System.Drawing.Size(0, 25)
         Me.lblDone.TabIndex = 10
@@ -139,80 +117,80 @@ Partial Class Form1
         'txtSorted
         '
         Me.txtSorted.BackColor = System.Drawing.Color.LightGreen
-        Me.txtSorted.Location = New System.Drawing.Point(228, 450)
+        Me.txtSorted.ForeColor = System.Drawing.Color.DarkOliveGreen
+        Me.txtSorted.Location = New System.Drawing.Point(991, 226)
         Me.txtSorted.Name = "txtSorted"
-        Me.txtSorted.Size = New System.Drawing.Size(283, 30)
+        Me.txtSorted.Size = New System.Drawing.Size(119, 30)
         Me.txtSorted.TabIndex = 12
+        Me.txtSorted.Text = "Sorted"
+        Me.txtSorted.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'txtCmp1
         '
         Me.txtCmp1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(239, Byte), Integer), CType(CType(113, Byte), Integer))
-        Me.txtCmp1.Location = New System.Drawing.Point(390, 509)
+        Me.txtCmp1.Location = New System.Drawing.Point(306, 188)
         Me.txtCmp1.Name = "txtCmp1"
-        Me.txtCmp1.Size = New System.Drawing.Size(156, 30)
+        Me.txtCmp1.ReadOnly = True
+        Me.txtCmp1.Size = New System.Drawing.Size(256, 30)
         Me.txtCmp1.TabIndex = 13
+        Me.txtCmp1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'txtCmp2
         '
         Me.txtCmp2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(239, Byte), Integer), CType(CType(113, Byte), Integer))
-        Me.txtCmp2.Location = New System.Drawing.Point(652, 509)
+        Me.txtCmp2.Location = New System.Drawing.Point(695, 188)
         Me.txtCmp2.Name = "txtCmp2"
-        Me.txtCmp2.Size = New System.Drawing.Size(249, 30)
+        Me.txtCmp2.ReadOnly = True
+        Me.txtCmp2.Size = New System.Drawing.Size(241, 30)
         Me.txtCmp2.TabIndex = 14
+        Me.txtCmp2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'txtUnSorted
         '
         Me.txtUnSorted.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(130, Byte), Integer), CType(CType(119, Byte), Integer))
-        Me.txtUnSorted.Location = New System.Drawing.Point(533, 450)
+        Me.txtUnSorted.ForeColor = System.Drawing.Color.DarkRed
+        Me.txtUnSorted.Location = New System.Drawing.Point(991, 276)
         Me.txtUnSorted.Name = "txtUnSorted"
-        Me.txtUnSorted.Size = New System.Drawing.Size(409, 30)
+        Me.txtUnSorted.Size = New System.Drawing.Size(119, 30)
         Me.txtUnSorted.TabIndex = 15
+        Me.txtUnSorted.Text = "Unsorted"
+        Me.txtUnSorted.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
-        'lblSorted
+        'lblLegend
         '
-        Me.lblSorted.AutoSize = True
-        Me.lblSorted.Location = New System.Drawing.Point(131, 453)
-        Me.lblSorted.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.lblSorted.Name = "lblSorted"
-        Me.lblSorted.Size = New System.Drawing.Size(70, 25)
-        Me.lblSorted.TabIndex = 16
-        Me.lblSorted.Text = "Sorted"
-        '
-        'lblUnsorted
-        '
-        Me.lblUnsorted.AutoSize = True
-        Me.lblUnsorted.Location = New System.Drawing.Point(964, 453)
-        Me.lblUnsorted.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.lblUnsorted.Name = "lblUnsorted"
-        Me.lblUnsorted.Size = New System.Drawing.Size(91, 25)
-        Me.lblUnsorted.TabIndex = 17
-        Me.lblUnsorted.Text = "Unsorted"
+        Me.lblLegend.AutoSize = True
+        Me.lblLegend.Location = New System.Drawing.Point(1009, 181)
+        Me.lblLegend.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblLegend.Name = "lblLegend"
+        Me.lblLegend.Size = New System.Drawing.Size(78, 25)
+        Me.lblLegend.TabIndex = 17
+        Me.lblLegend.Text = "Legend"
         '
         'txtCmpRes
         '
-        Me.txtCmpRes.Location = New System.Drawing.Point(576, 509)
+        Me.txtCmpRes.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtCmpRes.Location = New System.Drawing.Point(605, 188)
         Me.txtCmpRes.Name = "txtCmpRes"
-        Me.txtCmpRes.Size = New System.Drawing.Size(48, 30)
+        Me.txtCmpRes.ReadOnly = True
+        Me.txtCmpRes.Size = New System.Drawing.Size(48, 23)
         Me.txtCmpRes.TabIndex = 18
+        Me.txtCmpRes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'lblCmpWith
         '
         Me.lblCmpWith.AutoSize = True
-        Me.lblCmpWith.Location = New System.Drawing.Point(166, 514)
+        Me.lblCmpWith.Location = New System.Drawing.Point(78, 188)
         Me.lblCmpWith.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblCmpWith.Name = "lblCmpWith"
         Me.lblCmpWith.Size = New System.Drawing.Size(192, 25)
         Me.lblCmpWith.TabIndex = 19
         Me.lblCmpWith.Text = "Current Comparision"
         '
-        'Timer1
-        '
-        Me.Timer1.Interval = 5000
-        '
         'txtOutput1
         '
-        Me.txtOutput1.Location = New System.Drawing.Point(228, 237)
+        Me.txtOutput1.Location = New System.Drawing.Point(228, 253)
         Me.txtOutput1.Name = "txtOutput1"
+        Me.txtOutput1.ReadOnly = True
         Me.txtOutput1.Size = New System.Drawing.Size(714, 25)
         Me.txtOutput1.TabIndex = 11
         Me.txtOutput1.Text = ""
@@ -221,11 +199,11 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
         Me.ClientSize = New System.Drawing.Size(1197, 584)
         Me.Controls.Add(Me.lblCmpWith)
         Me.Controls.Add(Me.txtCmpRes)
-        Me.Controls.Add(Me.lblUnsorted)
-        Me.Controls.Add(Me.lblSorted)
+        Me.Controls.Add(Me.lblLegend)
         Me.Controls.Add(Me.txtUnSorted)
         Me.Controls.Add(Me.txtCmp2)
         Me.Controls.Add(Me.txtCmp1)
@@ -239,8 +217,6 @@ Partial Class Form1
         Me.Controls.Add(Me.lblOutput)
         Me.Controls.Add(Me.txtArr)
         Me.Controls.Add(Me.lblArr)
-        Me.Controls.Add(Me.txtNum)
-        Me.Controls.Add(Me.lblNum)
         Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Form1"
@@ -249,8 +225,6 @@ Partial Class Form1
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents lblNum As System.Windows.Forms.Label
-    Friend WithEvents txtNum As System.Windows.Forms.TextBox
     Friend WithEvents btnSort As System.Windows.Forms.Button
     Friend WithEvents lblArr As System.Windows.Forms.Label
     Friend WithEvents txtArr As System.Windows.Forms.TextBox
@@ -263,11 +237,9 @@ Partial Class Form1
     Friend WithEvents txtCmp1 As System.Windows.Forms.TextBox
     Friend WithEvents txtCmp2 As System.Windows.Forms.TextBox
     Friend WithEvents txtUnSorted As System.Windows.Forms.TextBox
-    Friend WithEvents lblSorted As System.Windows.Forms.Label
-    Friend WithEvents lblUnsorted As System.Windows.Forms.Label
+    Friend WithEvents lblLegend As System.Windows.Forms.Label
     Friend WithEvents txtCmpRes As System.Windows.Forms.TextBox
     Friend WithEvents lblCmpWith As System.Windows.Forms.Label
-    Friend WithEvents Timer1 As System.Windows.Forms.Timer
     Friend WithEvents txtOutput1 As System.Windows.Forms.RichTextBox
 
 End Class
--- Lab1-Insertion_Sort/Form1.vb
+++ Lab1-Insertion_Sort/Form1.vb
@@ -1,24 +1,26 @@
 ﻿Public Class Form1
 
-    Dim a() As String = Nothing
+    Dim a() As String = {}
     Dim inp() As String = {}
     Dim len As Integer = 0
     Dim cnt As Integer = 1
     Dim arr As Decimal() = {}
     Dim Fail As Boolean = 0
-    Dim siz As Integer = 0
     Dim flgdec As Integer = 0
     Dim flgstr As Integer = 0
     Dim y As Integer = 1
     Dim x As Integer = 1
     Dim counter As Integer = 1
 
-
-
     Private Sub Clr()
-        txtNum.Clear()
+        'txtNum.Clear()
+
         txtArr.Clear()
+        txtArr.ReadOnly = False
         txtOutput1.Clear()
+        txtCmp1.Clear()
+        txtCmp2.Clear()
+        txtCmpRes.Clear()
         For i As Integer = 2 To counter - 1
             Me.Controls.Remove(Me.Controls.Find("txtOutput" & CStr(i), True)(0))
         Next
@@ -28,12 +30,18 @@
         cnt = 1
         len = 0
         Fail = 0
-        siz = 0
         flgdec = 0
         flgstr = 0
-        y = 1
-        btnNxt.Hide()
+        y = 253
+        x = 228
+        counter = 1
         btnSort.Show()
+        btnNxt.Hide()
+        Application.Restart()
+        'Me.Refresh()
+        'Me.Controls.Clear() 'removes all the controls on the form
+        'InitializeComponent() 'load all the controls again
+
     End Sub
 
     Private Sub InpFail()
@@ -60,52 +68,54 @@
             Dim j As Integer = cnt - 1
             Dim cur As String = inp(cnt)
             While j >= 0
+                txtCmp1.Text = cur
+                txtCmp2.Text = inp(j)
                 If String.Compare(cur, inp(j)) = -1 Then
                     inp(j + 1) = inp(j)
                     j -= 1
+                    txtCmpRes.Text = "<"
                 Else
+                    txtCmpRes.Text = ">="
                     Exit While
                 End If
+                Delay(0.5)
             End While
             inp(j + 1) = cur
             cnt += 1
 
         End If
         '''''''insertion sort end'''''''''''
-        'Dim txt() As String = Nothing
-        'For k As Integer = 0 To inp.Length - 1
-        '    txt(k) = CStr(inp(k))
-        'Next
-        Dim strt As Integer = 0
-        Dim fin As Integer = 0
-        TxtIndex(strt, fin, cnt - 2, inp)
-        'MessageBox.Show(strt)
-        'MessageBox.Show(fin)
-        'inp.SetValue(strt, fin)
-        'inp.BackColor()
-        'tmp = Nothing
         For k As Integer = 0 To inp.Length - 1
-            tmp &= CStr(inp(k)) & " "
+            tmp &= inp(k) & " "
         Next
         tmp &= Environment.NewLine
-        'If j <> cnt - 1 Then
-        txtOutput1.Text &= tmp
-        'txtOutput.Select(strt, fin - strt)
-        txtOutput1.SelectionStart = strt
-        txtOutput1.SelectionLength = fin - strt
 
-        txtOutput1.SelectionBackColor = Color.Green
-        txtOutput1.DeselectAll()
+        Dim txt() As String = {}
+        For k As Integer = 0 To inp.Length - 1
+            Array.Resize(txt, txt.Length + 1)
+            txt(k) = inp(k)
+        Next
+        Dim strt As Integer = 0
+        Dim fin As Integer = 0
+        TxtIndex(strt, fin, cnt - 1, txt)
 
-        'txtOutput.SetBackColor = Color.Aqua
+        If counter = 1 Then
+            txtOutput1.Text = tmp
+            txtOutput1.Select(strt, tmp.Length - 1)
+            txtOutput1.SelectionBackColor = Color.PaleVioletRed
+            txtOutput1.Select(strt, fin - strt)
+            txtOutput1.SelectionBackColor = Color.LightGreen
 
-
-        'End If
+            counter += 1
+        Else
+            outTextAdder(counter, tmp, strt, fin)
+            counter += 1
+        End If
 
     End Sub
 
     '''function to make new richtextboxex'''
-    Private Sub outTextAdder(i As Integer, tmp As String)
+    Private Sub outTextAdder(i As Integer, tmp As String, strt As Integer, fin As Integer)
         Dim txtName As String
         txtName = "txtOutput" & CStr(i)
         y += 25
@@ -113,9 +123,15 @@
         txt1.Name = txtName
         txt1.Width = 714
         txt1.Height = 25
+        txt1.ReadOnly = True
         Me.Controls.Add(txt1)
         txt1.Location = New Point(x, y)
         txt1.Text = tmp
+        txt1.Select(strt, fin - strt)
+        txt1.SelectionBackColor = Color.LightGreen
+        txt1.Select(fin, tmp.Length)
+        txt1.SelectionBackColor = Color.PaleVioletRed
+
     End Sub
 
     Sub Delay(ByVal dblSecs As Double)
@@ -131,7 +147,6 @@
 
     Private Sub SortDec()
         Dim tmp As String = Nothing
-        Dim cmp As String = Nothing
         If len > 1 Then
             Dim j As Integer = cnt - 1
             Dim cur As Decimal = arr(cnt)
@@ -149,7 +164,7 @@
                 End If
 
                 'System.Threading.Thread.Sleep(1000)
-                Delay(1)
+                Delay(0.5)
             End While
             arr(j + 1) = cur
             cnt += 1
@@ -157,71 +172,37 @@
 
         'txtCmp2.Text = cmp
         For k As Integer = 0 To arr.Length - 1
-            tmp &= CStr(arr(k)) & "; "
+            tmp &= CStr(arr(k)) & " "
         Next
         tmp &= Environment.NewLine
 
-        'If j <> cnt - 1 Then
+        Dim txt() As String = {}
+        For k As Integer = 0 To inp.Length - 1
+            Array.Resize(txt, txt.Length + 1)
+            txt(k) = CStr(arr(k))
+        Next
+        Dim strt As Integer = 0
+        Dim fin As Integer = 0
+        TxtIndex(strt, fin, cnt - 1, txt)
+
         If counter = 1 Then
             txtOutput1.Text = tmp
+            txtOutput1.Select(strt, tmp.Length - 1)
+            txtOutput1.SelectionBackColor = Color.PaleVioletRed
+            txtOutput1.Select(strt, fin - strt)
+            txtOutput1.SelectionBackColor = Color.LightGreen
+            
+            counter += 1
+        Else
+            outTextAdder(counter, tmp, strt, fin)
             counter += 1
         End If
-        outTextAdder(counter, tmp)
-        counter += 1
-        'End If
 
     End Sub
 
-    'Private Sub SortDec2(ParamArray num() As Decimal)
-    '    Dim tmp As String = Nothing
-    '    For i As Integer = 1 To num.Length - 1
-    '        Dim j As Integer = i - 1
-    '        Dim cur As Decimal = num(i)
-    '        While j >= 0
-    '            If cur < num(j) Then
-    '                num(j + 1) = num(j)
-    '                j -= 1
-    '            Else
-    '                Exit While
-    '            End If
-    '        End While
-    '        num(j + 1) = cur
-    '        For k As Integer = 0 To num.Length - 1
-    '            tmp &= CStr(num(k)) & "; "
-    '        Next
-    '        tmp &= Environment.NewLine
-    '    Next
-
-    '    For i As Integer = 0 To num.Length - 1
-    '        tmp &= CStr(num(i)) & "; "
-    '    Next
-    '    txtOutput.Text = tmp
-    'End Sub
-
+   
     Private Function Sorter()
         btnNxt.Hide()
-        Try
-            Dim sz As String = CStr(txtNum.Text)
-            If sz.Contains(".") Then
-                InpFail()
-                Clr()
-                Fail = 1
-            Else
-                siz = CInt(sz)
-            End If
-
-        Catch ex As System.InvalidCastException
-            InpFail()
-            Clr()
-            Fail = 1
-        Catch ex As Exception
-            MessageBox.Show("Unknown Error", "Error")
-            Console.Write("Unknown Error")
-            Clr()
-            Fail = 1
-
-        End Try
-
         a = Split(txtArr.Text(), " ")
 
         Dim tmp As String = Nothing
@@ -249,8 +230,9 @@
                     flgstr = 1
                 Else
                     InpFail()
-                    Clr()
                     Fail = 1
+                    Clr()
+                    'Return 1
                     Exit For
                 End If
             End If
@@ -258,25 +240,16 @@
             If flgdec = 1 And flgstr = 1 Then
                 MessageBox.Show("Input of mixed type", "Error")
                 Console.Write("Error: Input of mixed type")
-                Clr()
                 Fail = 1
+                Clr()
                 Exit For
+                'Return 1
             End If
 
             Array.Resize(inp, inp.Length + 1)
             inp(inp.Length - 1) = a(i)
             'tmp &= a(i) & "; "
         Next
-
-
-        If inp.Length <> siz Then
-            MessageBox.Show(inp.Length, "Error")
-            MessageBox.Show(siz, "Error")
-            MessageBox.Show("Incorrect number of entries", "Error")
-            Console.Write("Error: Incorrect number of entries")
-            Clr()
-            Fail = 1
-        End If
 
         For i As Integer = 0 To inp.Length - 1
             tmp &= inp(i) & "; "
@@ -286,7 +259,18 @@
         If flgdec = 1 And Fail = 0 Then
             For i As Integer = 0 To inp.Length - 1
                 Array.Resize(arr, arr.Length + 1)
-                arr(arr.Length - 1) = CDec(inp(i))
+                Try
+                    arr(arr.Length - 1) = CDec(inp(i))
+
+                Catch ex As Exception
+                    MessageBox.Show("Number overflow/underflow", "Error")
+                    Console.Write("Error: Number overflow/underflow")
+                    Fail = 1
+                    Clr()
+                    'Return 1
+                    Exit For
+                End Try
+
             Next
             SortDec()
         End If
@@ -307,6 +291,7 @@
     End Sub
 
     Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+        txtArr.ReadOnly = True
         Sorter()
     End Sub
 
@@ -315,13 +300,14 @@
     End Sub
 
     Private Sub btnNxt_Click(sender As Object, e As EventArgs) Handles btnNxt.Click
-
+        btnNxt.Hide()
         If cnt < len Then
             If flgdec = 1 Then
                 SortDec()
             ElseIf flgstr = 1 Then
                 SortStr()
             End If
+            btnNxt.Show()
         ElseIf cnt = len Then
             btnNxt.Hide()
         End If
@@ -329,20 +315,12 @@
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        y = 237
+        y = 253
         x = 228
     End Sub
 
     Private Sub lblOutput_Click(sender As Object, e As EventArgs) Handles lblOutput.Click
 
     End Sub
-
-    'Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-    '    If tim = 0 Then
-    '        Timer1.Enabled = False
-    '    End If
-    '    tim -= 1
-    'End Sub
-
 
 End Class
