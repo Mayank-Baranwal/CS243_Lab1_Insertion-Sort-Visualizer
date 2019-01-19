5a9073bab10f728049daa420279302a82b0ca78d
Mayank Baranwal <baranwalm2002@gmail.com>
2019-01-19 17:43:02 +0530

    Lab 1 Colour Prob

---
+++ Lab1-Insertion_Sort/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblNum = New System.Windows.Forms.Label()
+        Me.txtNum = New System.Windows.Forms.TextBox()
+        Me.btnSort = New System.Windows.Forms.Button()
+        Me.lblArr = New System.Windows.Forms.Label()
+        Me.txtArr = New System.Windows.Forms.TextBox()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClr = New System.Windows.Forms.Button()
+        Me.btnNxt = New System.Windows.Forms.Button()
+        Me.lblDone = New System.Windows.Forms.Label()
+        Me.txtOutput = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'lblNum
+        '
+        Me.lblNum.AutoSize = True
+        Me.lblNum.Location = New System.Drawing.Point(341, 31)
+        Me.lblNum.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblNum.Name = "lblNum"
+        Me.lblNum.Size = New System.Drawing.Size(283, 25)
+        Me.lblNum.TabIndex = 0
+        Me.lblNum.Text = "Enter Number of Entities to Sort"
+        '
+        'txtNum
+        '
+        Me.txtNum.Location = New System.Drawing.Point(652, 28)
+        Me.txtNum.Name = "txtNum"
+        Me.txtNum.Size = New System.Drawing.Size(74, 30)
+        Me.txtNum.TabIndex = 1
+        '
+        'btnSort
+        '
+        Me.btnSort.Location = New System.Drawing.Point(504, 166)
+        Me.btnSort.Name = "btnSort"
+        Me.btnSort.Size = New System.Drawing.Size(136, 59)
+        Me.btnSort.TabIndex = 2
+        Me.btnSort.Text = "Start Insertion Sort"
+        Me.btnSort.UseVisualStyleBackColor = True
+        '
+        'lblArr
+        '
+        Me.lblArr.AutoSize = True
+        Me.lblArr.Location = New System.Drawing.Point(78, 108)
+        Me.lblArr.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblArr.Name = "lblArr"
+        Me.lblArr.Size = New System.Drawing.Size(110, 25)
+        Me.lblArr.TabIndex = 3
+        Me.lblArr.Text = "Enter Array"
+        '
+        'txtArr
+        '
+        Me.txtArr.Font = New System.Drawing.Font("Calibri", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtArr.Location = New System.Drawing.Point(228, 98)
+        Me.txtArr.Multiline = True
+        Me.txtArr.Name = "txtArr"
+        Me.txtArr.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtArr.Size = New System.Drawing.Size(714, 46)
+        Me.txtArr.TabIndex = 4
+        '
+        'lblOutput
+        '
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.Location = New System.Drawing.Point(78, 343)
+        Me.lblOutput.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(71, 25)
+        Me.lblOutput.TabIndex = 5
+        Me.lblOutput.Text = "Output"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(714, 166)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(92, 59)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClr
+        '
+        Me.btnClr.Location = New System.Drawing.Point(331, 166)
+        Me.btnClr.Name = "btnClr"
+        Me.btnClr.Size = New System.Drawing.Size(92, 59)
+        Me.btnClr.TabIndex = 8
+        Me.btnClr.Text = "Clear"
+        Me.btnClr.UseVisualStyleBackColor = True
+        '
+        'btnNxt
+        '
+        Me.btnNxt.Location = New System.Drawing.Point(504, 166)
+        Me.btnNxt.Name = "btnNxt"
+        Me.btnNxt.Size = New System.Drawing.Size(136, 59)
+        Me.btnNxt.TabIndex = 9
+        Me.btnNxt.Text = "Next Step"
+        Me.btnNxt.UseVisualStyleBackColor = True
+        '
+        'lblDone
+        '
+        Me.lblDone.AutoSize = True
+        Me.lblDone.Location = New System.Drawing.Point(709, 265)
+        Me.lblDone.Name = "lblDone"
+        Me.lblDone.Size = New System.Drawing.Size(0, 25)
+        Me.lblDone.TabIndex = 10
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(228, 262)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(714, 220)
+        Me.txtOutput.TabIndex = 11
+        Me.txtOutput.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1043, 510)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.lblDone)
+        Me.Controls.Add(Me.btnSort)
+        Me.Controls.Add(Me.btnNxt)
+        Me.Controls.Add(Me.btnClr)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.txtArr)
+        Me.Controls.Add(Me.lblArr)
+        Me.Controls.Add(Me.txtNum)
+        Me.Controls.Add(Me.lblNum)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblNum As System.Windows.Forms.Label
+    Friend WithEvents txtNum As System.Windows.Forms.TextBox
+    Friend WithEvents btnSort As System.Windows.Forms.Button
+    Friend WithEvents lblArr As System.Windows.Forms.Label
+    Friend WithEvents txtArr As System.Windows.Forms.TextBox
+    Friend WithEvents lblOutput As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClr As System.Windows.Forms.Button
+    Friend WithEvents btnNxt As System.Windows.Forms.Button
+    Friend WithEvents lblDone As System.Windows.Forms.Label
+    Friend WithEvents txtOutput As System.Windows.Forms.RichTextBox
+
+End Class
---
+++ Lab1-Insertion_Sort/Form1.vb
@@ -0,0 +1,282 @@
+﻿Public Class Form1
+
+    Dim a() As String = Nothing
+    Dim inp() As String = {}
+    Dim len As Integer = 0
+    Dim cnt As Integer = 1
+    Dim arr As Decimal() = {}
+    Dim Fail As Boolean = 0
+    Dim siz As Integer = 0
+    Dim flgdec As Integer = 0
+    Dim flgstr As Integer = 0
+
+
+    Private Sub Clr()
+        txtNum.Clear()
+        txtArr.Clear()
+        txtOutput.Clear()
+        a = {}
+        inp = {}
+        arr = {}
+        cnt = 1
+        len = 0
+        Fail = 0
+        siz = 0
+        flgdec = 0
+        flgstr = 0
+        btnSort.Show()
+    End Sub
+
+    Private Sub InpFail()
+        MessageBox.Show("Enter valid input", "Error")
+        Console.Write("Error: Invalid input type")
+    End Sub
+
+    Private Sub TxtIndex(ByRef strt As Integer, ByRef fin As Integer, cnt As Integer, ParamArray Op() As String)
+        Dim ind As Integer = 0
+        For i As Integer = 0 To cnt
+            ind += (Op(i).Length + 1)
+        Next
+        If cnt = Op.Length() Then
+            ind -= 1
+        End If
+        'MessageBox.Show(ind)
+        fin = strt + ind
+    End Sub
+
+    Private Sub SortStr()
+        Dim tmp As String = Nothing
+        If len > 1 Then
+            Dim j As Integer = cnt - 1
+            Dim cur As String = inp(cnt)
+            While j >= 0
+                If String.Compare(cur, inp(j)) = -1 Then
+                    inp(j + 1) = inp(j)
+                    j -= 1
+                Else
+                    Exit While
+                End If
+            End While
+            inp(j + 1) = cur
+            cnt += 1
+
+        End If
+
+        'Dim txt() As String = Nothing
+        'For k As Integer = 0 To inp.Length - 1
+        '    txt(k) = CStr(inp(k))
+        'Next
+        Dim strt As Integer = 0
+        Dim fin As Integer = 0
+        TxtIndex(strt, fin, cnt - 2, inp)
+        'MessageBox.Show(strt)
+        'MessageBox.Show(fin)
+        'inp.SetValue(strt, fin)
+        'inp.BackColor()
+        'tmp = Nothing
+        For k As Integer = 0 To inp.Length - 1
+            tmp &= CStr(inp(k)) & " "
+        Next
+        tmp &= Environment.NewLine
+        'If j <> cnt - 1 Then
+        txtOutput.Text &= tmp
+        'txtOutput.Select(strt, fin - strt)
+        txtOutput.SelectionStart = strt
+        txtOutput.SelectionLength = fin - strt
+
+        txtOutput.SelectionBackColor = Color.Green
+        txtOutput.DeselectAll()
+
+        'txtOutput.SetBackColor = Color.Aqua
+
+
+        'End If
+
+    End Sub
+
+    Private Sub SortDec()
+        Dim tmp As String = Nothing
+        If len > 1 Then
+            Dim j As Integer = cnt - 1
+            Dim cur As Decimal = arr(cnt)
+            While j >= 0
+                If cur < arr(j) Then
+                    arr(j + 1) = arr(j)
+                    j -= 1
+                Else
+                    Exit While
+                End If
+            End While
+            arr(j + 1) = cur
+            cnt += 1
+
+        End If
+
+        For k As Integer = 0 To arr.Length - 1
+            tmp &= CStr(arr(k)) & "; "
+        Next
+        tmp &= Environment.NewLine
+
+        'If j <> cnt - 1 Then
+        txtOutput.Text &= tmp
+        'End If
+
+    End Sub
+
+    'Private Sub SortDec2(ParamArray num() As Decimal)
+    '    Dim tmp As String = Nothing
+    '    For i As Integer = 1 To num.Length - 1
+    '        Dim j As Integer = i - 1
+    '        Dim cur As Decimal = num(i)
+    '        While j >= 0
+    '            If cur < num(j) Then
+    '                num(j + 1) = num(j)
+    '                j -= 1
+    '            Else
+    '                Exit While
+    '            End If
+    '        End While
+    '        num(j + 1) = cur
+    '        For k As Integer = 0 To num.Length - 1
+    '            tmp &= CStr(num(k)) & "; "
+    '        Next
+    '        tmp &= Environment.NewLine
+    '    Next
+
+    '    For i As Integer = 0 To num.Length - 1
+    '        tmp &= CStr(num(i)) & "; "
+    '    Next
+    '    txtOutput.Text = tmp
+    'End Sub
+
+    Private Function Sorter()
+        btnNxt.Hide()
+        Try
+            Dim sz As String = CStr(txtNum.Text)
+            If sz.Contains(".") Then
+                InpFail()
+                Clr()
+                Fail = 1
+            Else
+                siz = CInt(sz)
+            End If
+
+        Catch ex As System.InvalidCastException
+            InpFail()
+            Clr()
+            Fail = 1
+        Catch ex As Exception
+            MessageBox.Show("Unknown Error", "Error")
+            Console.Write("Unknown Error")
+            Clr()
+            Fail = 1
+
+        End Try
+
+        a = Split(txtArr.Text(), " ")
+
+        Dim tmp As String = Nothing
+        'Dim cnt As Integer = 0
+
+        For i As Integer = 0 To a.Length - 1
+            If a(i).Trim = "" Then
+                Continue For
+            End If
+            Dim isLetter As Boolean = True
+            If IsNumeric(a(i)) Then
+                flgdec = 1
+                'tmp &= flgdec & " "
+            Else
+                For j = 1 To a(i).Length
+                    Select Case Asc(Mid(a(i), j, 1))
+                        Case 65 To 90, 97 To 122
+                            Continue For
+                        Case Else
+                            isLetter = False
+                            Exit For
+                    End Select
+                Next
+                If isLetter = True Then
+                    flgstr = 1
+                Else
+                    InpFail()
+                    Clr()
+                    Fail = 1
+                    Exit For
+                End If
+            End If
+
+            If flgdec = 1 And flgstr = 1 Then
+                MessageBox.Show("Input of mixed type", "Error")
+                Console.Write("Error: Input of mixed type")
+                Clr()
+                Fail = 1
+                Exit For
+            End If
+
+            Array.Resize(inp, inp.Length + 1)
+            inp(inp.Length - 1) = a(i)
+            'tmp &= a(i) & "; "
+        Next
+
+
+        If inp.Length <> siz Then
+            MessageBox.Show(inp.Length, "Error")
+            MessageBox.Show(siz, "Error")
+            MessageBox.Show("Incorrect number of entries", "Error")
+            Console.Write("Error: Incorrect number of entries")
+            Clr()
+            Fail = 1
+        End If
+
+        For i As Integer = 0 To inp.Length - 1
+            tmp &= inp(i) & "; "
+        Next
+
+        len = inp.Length
+        If flgdec = 1 And Fail = 0 Then
+            For i As Integer = 0 To inp.Length - 1
+                Array.Resize(arr, arr.Length + 1)
+                arr(arr.Length - 1) = CDec(inp(i))
+            Next
+            SortDec()
+        End If
+        If flgstr = 1 And Fail = 0 Then
+            SortStr()
+        End If
+        btnSort.Hide()
+        btnNxt.Show()
+        'If Fail = 0 Then
+        '    txtOutput.Text = tmp
+        'End If
+
+        Return 0
+    End Function
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+        Sorter()
+    End Sub
+
+    Private Sub btnClr_Click(sender As Object, e As EventArgs) Handles btnClr.Click
+        Clr()
+    End Sub
+
+    Private Sub btnNxt_Click(sender As Object, e As EventArgs) Handles btnNxt.Click
+
+        If cnt < len Then
+            If flgdec = 1 Then
+                SortDec()
+            ElseIf flgstr = 1 Then
+                SortStr()
+            End If
+        ElseIf cnt = len Then
+            btnNxt.Hide()
+        End If
+
+    End Sub
+
+End Class
